54d006c7558d062f3a11baf308703e3f6d71905b
Yingkan Chen <cyk@mytum.de>
2019-08-13 00:06:12 +0800

    improved single file processing

--- main_1Ch.py
+++ main_1Ch.py
@@ -6,12 +6,24 @@ import scipy.io as sio
 def data_preprocessing(data_path):
     f = os.listdir(data_path)
     for mat_file in f:
-        a =single_file_proc(os.path.join(data_path, mat_file))
-    return training_data
+        try:
+            a =single_file_proc(os.path.join(data_path, mat_file))
+        except:
+            print('%s has problem.' % mat_file)
+    return a
 
 
 def single_file_proc(single_data_path):
-    sio.loadmat(single_data_path)
+    frame_tested = 5
+    tmp = sio.loadmat(single_data_path)
+    kMemPol = tmp['kMemPol'][0][0]
+    for frame in range(0, frame_tested):
+        frame_data = np.zeros(shape=[12, 180, 74]) + 1j * np.zeros(shape=[12, 180, 74])
+        for tri in range(0, 11):
+            frame_data[tri, :,:] = np.transpose(tmp['RxPayloadArray_NN'][tri][0][0+frame*74 : 74+frame*74 , :])
+
+        frame_calc =  (frame + kMemPol) if (frame + kMemPol) < 10 else (frame + kMemPol) - 9
+        sio.savemat('%s\\frame_wise\\%s_frame_%d.mat'%(os.path.dirname(os.path.dirname(single_data_path)), os.path.basename(single_data_path).split('.mat')[0],frame_calc), {'frame_data':frame_data})
     # data: preamble + payload
     # data normalization to sqrt(2)
     # label: premable + payload
@@ -19,8 +31,8 @@ def single_file_proc(single_data_path):
 
 
 if __name__ == '__main__':
-    data_path = r'C:\\FMF_NN_EQ\\'
+    data_path = r'C:\\FMF_NN_EQ\\ori_form\\'
     training_data = data_preprocessing(data_path)
-    model = VDSR(d=8, s=4, m=2, input_shape=[10, 180, 12]).build_model()
-    train_model(training_data, model, tf.train.AdamOptimizer(0.001))
+    #model = VDSR(d=8, s=4, m=2, input_shape=[10, 180, 12]).build_model()
+    #train_model(training_data, model, tf.train.AdamOptimizer(0.001))
 
