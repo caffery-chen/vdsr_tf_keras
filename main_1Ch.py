4aa98463cf1a4828d3e351e420ee5b5d91f904a6
Yingkan Chen <cyk@mytum.de>
2019-09-27 00:37:16 +0800

    critical change in the label data preparation

--- main_1Ch.py
+++ main_1Ch.py
@@ -3,8 +3,8 @@ from nn_utility import *
 from VDSR import VDSR
 import numpy as np
 import scipy.io as sio
-import moxing as mox
-mox.file.shift('os', 'mox')
+# import moxing as mox
+# mox.file.shift('os', 'mox')
 import pdb
 import matplotlib.pyplot as plt
 import datetime
@@ -26,14 +26,17 @@ def single_file_proc(single_data_path):
     kMemPol = tmp['kMemPol'][0][0]
     for frame in range(0, frame_tested):
         y_data = x_data = np.zeros(shape=[74 + 24, 180, 12]) + 1j * np.zeros(shape=[74 + 24, 180, 12])
-        for tri in range(0, 11):
+        for tri in range(0, 12):
             x_data[0:24,:,  tri] =tmp['RxPreambleArray_NN'][tri][0][0 + frame * 24: 24 + frame * 24, :]
             x_data[24:98,:,  tri] = tmp['RxPayloadArray_NN'][tri][0][0 + frame * 74 : 74 + frame * 74 , :]
 
             y_data[0:24,:,  tri] = tmp['TxPreambleArray_NN'][tri][0][0 + frame * 24: 24 + frame * 24, :]
             y_data[24:98,:,  tri] = tmp['TxPayloadArray_NN'][tri][0][0 + frame * 74 : 74 + frame * 74 , :]
 
-        frame_calc =  (frame + kMemPol) if (frame + kMemPol) <= 10 else (frame + kMemPol) - 10
+        y_data = np.reshape(y_data, [98, 1, 180, 12])
+        y_data = np.concatenate([np.real(y_data[0:98, :, :, :]), np.imag(y_data[0:98, :, :, :])], axis=2)
+        frame_calc = calc_ber(y_data)
+
         sio.savemat('%s\\complete_frame\\%s_cframe_%d.mat'%(os.path.dirname(os.path.dirname(single_data_path)),
                                                           os.path.basename(single_data_path).split('.mat')[0],frame_calc), {'x_data':x_data, 'y_data':y_data})
 
@@ -176,7 +179,6 @@ def calc_ber(y_predict):
         
         for Tri in range(0,12):
             output_bits = qam_demod(fut[Tri * 2 : 52 + Tri * 2,:,Tri])
-            pdb.set_trace()
             #sio.savemat('./py_output_bits.mat',{'outputs_bits':output_bits})
             #sio.savemat('./py_output_sym.mat',{'outputs_sym': fut[Tri * 2 : 52 + Tri * 2,:,Tri]})
             
@@ -197,33 +199,34 @@ def calc_ber(y_predict):
             
             print('Matched Frame Index %d; Frame#%d/%d; Tri:%d/%d: BER = %e' % (k, i+1, frames_tested, Tri+1, 12, best_ber))
             ber[i, Tri] = best_ber
+    return k
             
 def chkmkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
         
-#if __name__ == '__main__':
-    #np.reshape([[[1,2,3],[1,3,4]],[[3,4,5],[5,6,7]]], [4,1,3])
-    # data_path = r'C:\\FMF_NN_EQ\\ori_form'
-    # training_data = data_preprocessing(data_path)
-def main():
-    
-    now = datetime.datetime.now()      
-    #print(flags.data_url)
-    #print(flags.train_url)
-    log_dir = r's3://obs-fmf-eq/model/%d-%d-%d' % (now.year, now.month, now.day)
-    chkmkdir(log_dir)
-    
-    data_path = r's3://obs-fmf-eq/complete_frame'    
-    
-    training_data = get_data_by_frame(data_path, 1)
-    test_data = get_test_data_by_frame(data_path, 5)#{'test_data': training_data['train_data'], 'test_label': training_data['train_label']}
-    val_data = get_val_data_by_frame(data_path, [3])#{'val_data': training_data['train_data'], 'val_label': training_data['train_label']}
-    
-    model = VDSR(d=64, s=32, m=5, input_shape=[1, 360, 12]).build_model()
-    
-    train_model(training_data, test_data, val_data, model, tf.train.AdamOptimizer(0.001),flags.train_url)
-    
 if __name__ == '__main__':
-    main()
+    #np.reshape([[[1,2,3],[1,3,4]],[[3,4,5],[5,6,7]]], [4,1,3])
+    data_path = r'C:\\FMF_NN_EQ\\ori_frame'
+    training_data = data_preprocessing(data_path)
+# def main():
+#
+#     now = datetime.datetime.now()
+#     #print(flags.data_url)
+#     #print(flags.train_url)
+#     log_dir = r's3://obs-fmf-eq/model/%d-%d-%d' % (now.year, now.month, now.day)
+#     chkmkdir(log_dir)
+#
+#     data_path = r's3://obs-fmf-eq/complete_frame'
+#
+#     training_data = get_data_by_frame(data_path, 1)
+#     test_data = get_test_data_by_frame(data_path, 5)#{'test_data': training_data['train_data'], 'test_label': training_data['train_label']}
+#     val_data = get_val_data_by_frame(data_path, [3])#{'val_data': training_data['train_data'], 'val_label': training_data['train_label']}
+#
+#     model = VDSR(d=64, s=32, m=5, input_shape=[1, 360, 12]).build_model()
+#
+#     train_model(training_data, test_data, val_data, model, tf.train.AdamOptimizer(0.001), log_dir)
+#
+# if __name__ == '__main__':
+#     main()
 
