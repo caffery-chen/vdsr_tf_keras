2c81de36a1d3dec52057c9b39d8cffd423bb009d
Yingkan Chen <cyk@mytum.de>
2019-09-26 22:09:00 +0800

    fixed single_file_proc

--- main_1Ch.py
+++ main_1Ch.py
@@ -3,11 +3,12 @@ from nn_utility import *
 from VDSR import VDSR
 import numpy as np
 import scipy.io as sio
-# import moxing as mox
-# mox.file.shift('os', 'mox')
+import moxing as mox
+mox.file.shift('os', 'mox')
 import pdb
 import matplotlib.pyplot as plt
 import datetime
+import tensorflow as tf
 
 def data_preprocessing(data_path):
     f = os.listdir(data_path)
@@ -50,17 +51,12 @@ def get_data_by_frame(file_path, frame_idx):
     x_data = np.reshape(x_data, [L * 98, 1, 180,12])
     y_data = np.reshape(y_data, [L * 98, 1, 180,12])
 
-    L = np.size(x_data, 0)
-    train_len = int(np.ceil(0.8 * L))
-    test_len = int(L-train_len)
+    L = np.size(x_data, 0)   
 
-    x_train = np.concatenate([np.real(x_data[0:train_len, :, :, :]), np.imag(x_data[0:train_len, :, :, :])], axis = 2)
-    y_train = np.concatenate([np.real(y_data[0:train_len, :, :, :]), np.imag(y_data[0:train_len, :, :, :])], axis = 2)
+    x_train = np.concatenate([np.real(x_data[0:L, :, :, :]), np.imag(x_data[0:L, :, :, :])], axis = 2)
+    y_train = np.concatenate([np.real(y_data[0:L, :, :, :]), np.imag(y_data[0:L, :, :, :])], axis = 2)
 
-    x_test = np.concatenate([np.real(x_data[train_len:train_len + test_len, :, :, :]), np.imag(x_data[train_len:train_len + test_len, :, :, :])], axis = 2)
-    y_test = np.concatenate([np.real(y_data[train_len:train_len + test_len, :, :, :]), np.imag(y_data[train_len:train_len + test_len, :, :, :])], axis = 2)
-
-    training_data = {'train_data':x_train, 'train_label':y_train, 'test_data':x_test, 'test_label':y_test}
+    training_data = {'train_data':x_train, 'train_label':y_train}
     return training_data
 
 def get_test_data_by_frame(file_path, frame_idx):
@@ -88,7 +84,7 @@ def get_val_data_by_frame(file_path, frame_idx):
     f = os.listdir(file_path)
     x_data = []
     y_data = []
-    for mat_file in f:
+    for mat_file in f[0:1000]:
         for frame_idx_iter in frame_idx:
             if '_cframe_%d' % frame_idx_iter in mat_file:
                 xx = sio.loadmat(os.path.join(file_path, mat_file))
@@ -121,7 +117,7 @@ def model_validation(data_path, chkpt_path, frame_idx):
     
     val_data = get_val_data_by_frame(data_path, frame_idx)['val_data']    
     y_predict = model.predict(val_data, steps = 4)    
-    ber = calc_ber(y_predict, frame_idx[0])
+    ber = calc_ber(y_predict)
     
 #     print('predicted')
 #     real_part = np.reshape(y_predict[:,0,0:180,:], [-1])
@@ -166,7 +162,7 @@ def qam_demod(c_number):
     
     return output_bits    
     
-def calc_ber(y_predict, frame_idx):
+def calc_ber(y_predict):
     
     frame_length = 98
     rx_symbol = y_predict[:,0,0:180,:] + 1j * y_predict[:,0,180:360,:]
@@ -180,6 +176,7 @@ def calc_ber(y_predict, frame_idx):
         
         for Tri in range(0,12):
             output_bits = qam_demod(fut[Tri * 2 : 52 + Tri * 2,:,Tri])
+            pdb.set_trace()
             #sio.savemat('./py_output_bits.mat',{'outputs_bits':output_bits})
             #sio.savemat('./py_output_sym.mat',{'outputs_sym': fut[Tri * 2 : 52 + Tri * 2,:,Tri]})
             
@@ -198,33 +195,35 @@ def calc_ber(y_predict, frame_idx):
                     best_ber = ber_frame_idx
                     k=frame_idx
             
-            print('Matched Frame Index %d; Frame#%d/%d; Tri:%d/%d: BER = %e' % (k, i, frames_tested, Tri+1, 12, best_ber))
+            print('Matched Frame Index %d; Frame#%d/%d; Tri:%d/%d: BER = %e' % (k, i+1, frames_tested, Tri+1, 12, best_ber))
             ber[i, Tri] = best_ber
             
 def chkmkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
         
-if __name__ == '__main__':
+#if __name__ == '__main__':
     #np.reshape([[[1,2,3],[1,3,4]],[[3,4,5],[5,6,7]]], [4,1,3])
-    data_path = r'C:\\FMF_NN_EQ\\ori_frame'
-    training_data = data_preprocessing(data_path)
-# def main():
-#
-#     now = datetime.datetime.now()
-#     log_dir = r's3://obs-fmf-eq/model/%d-%d-%d' % (now.year, now.month, now.day)
-#     chkmkdir(log_dir)
-#
-#     data_path = r's3://obs-fmf-eq/complete_frame_new'
-#
-#     training_data = get_data_by_frame(data_path, 1)
-#     test_data = get_test_data_by_frame(data_path, 5)#{'test_data': training_data['train_data'], 'test_label': training_data['train_label']}
-#     val_data = get_val_data_by_frame(data_path, [3])#{'val_data': training_data['train_data'], 'val_label': training_data['train_label']}
-#
-#     model = VDSR(d=64, s=32, m=5, input_shape=[1, 360, 12]).build_model()
-#
-#     train_model(training_data, test_data, val_data, model, tf.train.AdamOptimizer(0.001),log_dir)
-#
-# if __name__ == '__main__':
-#     main()
+    # data_path = r'C:\\FMF_NN_EQ\\ori_form'
+    # training_data = data_preprocessing(data_path)
+def main():
+    
+    now = datetime.datetime.now()      
+    #print(flags.data_url)
+    #print(flags.train_url)
+    log_dir = r's3://obs-fmf-eq/model/%d-%d-%d' % (now.year, now.month, now.day)
+    chkmkdir(log_dir)
+    
+    data_path = r's3://obs-fmf-eq/complete_frame'    
+    
+    training_data = get_data_by_frame(data_path, 1)
+    test_data = get_test_data_by_frame(data_path, 5)#{'test_data': training_data['train_data'], 'test_label': training_data['train_label']}
+    val_data = get_val_data_by_frame(data_path, [3])#{'val_data': training_data['train_data'], 'val_label': training_data['train_label']}
+    
+    model = VDSR(d=64, s=32, m=5, input_shape=[1, 360, 12]).build_model()
+    
+    train_model(training_data, test_data, val_data, model, tf.train.AdamOptimizer(0.001),flags.train_url)
+    
+if __name__ == '__main__':
+    main()
 
